8404edf6801aa79a3fa09bdcfa352a9ecf529bc3
Mohsen <rubi@rubis-MacBook-Air.local>
2018-11-08 20:33:20 -0500

    screen function created

---
+++ tests/test_screen.py
@@ -0,0 +1,10 @@
+# =============================================================================
+# Created By  : Mohsen Malmir
+# Created Date: Thu Nov 08 8:27 PM EST 2018
+# Purpose     : this file implements the test of screen functionality
+# =============================================================================
+
+
+
+def test_grab():
+
